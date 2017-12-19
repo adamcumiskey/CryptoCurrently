70131a1f1fbfdb25e93f1b1560eb7940a8c1cae6
Adam <adam.cumiskey@gmail.com>
2017-12-19 09:34:45 -0500

    Comments and refactoring

--- src/cryptocurrently/core.cljs
+++ src/cryptocurrently/core.cljs
@@ -6,14 +6,11 @@
             [goog.string :as gstring]
             [goog.string.format]))
 
-(defonce btc-price (r/atom 0))
-(defonce ltc-price (r/atom 0))
-(defonce eth-price (r/atom 0))
-
 ;; -------------------------
 ;; Websockets
 
 (defn connect [ch url]
+  "Establish connection to websocket and set value of ch"
   (go 
     (let [{:keys [ws-channel error]} (<! (ws-ch url {:format :json}))]
       (if-not error
@@ -21,10 +18,21 @@
         (js/console.log error)))))
 
 (defn subscribe [ch product-ids events]
+  "Subscribe to events for the given product-ids.
+   Required to be sent within 5s of connecting."
   (go
     (>! ch {:type "subscribe"
             :product_ids product-ids
             :channels events}))) 
+
+(def ws-feed-url "wss://ws-feed.gdax.com")
+(def products ["BTC-USD" "ETH-USD" "LTC-USD"])
+(def events ["ticker"])
+
+;; Store prices in Reagent atoms for UI binding
+(defonce btc-price (r/atom 0))
+(defonce ltc-price (r/atom 0))
+(defonce eth-price (r/atom 0))
 
 (defmulti event-handler (fn [message log-enabled] [(message "type") (message "product_id")]))
 (defmethod event-handler ["ticker" "BTC-USD"] [message log-enabled]
@@ -35,34 +43,34 @@
   (when (true? log-enabled) (prn (str "Litecoin: $" (message "price")))))
 (defmethod event-handler ["ticker" "ETH-USD"] [message log-enabled]
   (reset! eth-price (message "price"))
-  (when (true? log-enabled) (prn (str "Etherium: $" (message "price")))))
+  (when (true? log-enabled) (prn (str "Ethereum: $" (message "price")))))
 (defmethod event-handler :default [message log-enabled]
-  (when (true? log-enabled) (prn "Unhandled message: " message)))
+  (when (true? log-enabled) (prn (str "Unhandled message: " message))))
 
 (defn handle-events [ch]
+  "Dispatch messages from ch to the event-handler"
   (go-loop []
     (when-let [{:keys [message]} (<! ch)]
       (event-handler message true)
       (recur))))
 
-(def ws-feed-url "wss://ws-feed.gdax.com")
-(defonce channel (atom (chan)))
-(def currencies ["BTC-USD" "ETH-USD" "LTC-USD"])
-(def events ["ticker"])
-
-(add-watch channel nil
-  (fn [k r os ch]
-    (if-not (nil? ch)
-      (do
-        (handle-events ch)
-        (subscribe ch currencies events)))))
-
-(connect channel ws-feed-url)
+(defn start-updating [ch products events]
+  "Connect to the websocket feed.
+   On success, start the event-hander and subscribe
+   to a feed."
+  (add-watch ch nil
+    (fn [k r os ch]
+      (if-not (nil? ch)
+        (do
+          (handle-events ch)
+          (subscribe ch products events)))))
+  (connect ch ws-feed-url))
 
 ;; -------------------------
 ;; Views
 
 (defn currency-row [name price]
+  "Div for a single currency"
   [:div
    {:style {:padding 50.0
             :text-align :center}}
@@ -77,13 +85,16 @@
                :flex-direction :row}}
       [currency-row "Bitcoin" btc-price]
       [currency-row "Litecoin" ltc-price]
-      [currency-row "Etherium" eth-price]]])
+      [currency-row "Ethereum" eth-price]]])
 
 ;; -------------------------
 ;; Initialize app
+
+(defonce channel (atom (chan)))
 
 (defn mount-root []
   (r/render [home-page] (.getElementById js/document "app")))
 
 (defn init! []
-  (mount-root))
+  (mount-root)
+  (start-updating channel products events))
